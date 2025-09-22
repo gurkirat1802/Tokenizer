60fca1d747d1ecf18d6c1251a895338efa3bcb38
gurkirat1802 <85217914+gurkirat1802@users.noreply.github.com>
2025-09-22 13:27:24 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AsciiTokenizer from "./components/AsciiTokenizer"
+
+function App() {
+
+  return (
+    <>
+      <p className="text-center py-4 font-bold text-4xl text-white bg-gradient-to-r from-cyan-600 via-blue-600 to-purple-700">
+        CUSTOM ASCII TOKENIZER
+      </p>
+      <AsciiTokenizer />
+    </>
+  )
+}
+
+export default App
